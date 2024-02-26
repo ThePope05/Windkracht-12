71c6bccce30f6fa0a568c0c59f44113a2b92bbb4
ThePope05 <swobben05@gmail.com>
2024-02-26 21:47:14 +0100

    Update route-button styling

--- resources/views/components/route-button.blade.php
+++ resources/views/components/route-button.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ $attributes['route'] }}" class="bg-gray-600 hover:bg-gray-300 text-slate-800 px-6 py-2 rounded-lg transition-colors">
+<a href="{{ $attributes['route'] }}" class="bg-gray-600 hover:bg-gray-300 text-slate-800 px-6 py-2 rounded-lg transition-colors {{ ($attributes['route'] == request()->url()) ? 'text-orange-400' : '' }}">
     {{ $slot }}
 </a>
