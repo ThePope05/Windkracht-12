6ab8257331632877df788f2b5e64a618815b71bf
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2024-02-27 10:30:41 +0100

    Update tailwind.config.js and welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,3 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Welcome to Laravel 8
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="p-6 sm:px-20 bg-slate-800">
-                    <div class="mt-8 text-2xl text-orange-500">
-                        Welcome to your Laravel 8 application!
-                    </div>
-
-                    <div class="mt-6 text-gray-500">
-                        Laravel 8 is a powerful framework that provides a lot of features for building web applications. It is a modern PHP framework that is built on the MVC architecture. It is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller architectural pattern.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </x-app-layout>
