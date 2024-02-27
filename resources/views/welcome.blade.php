bc2d35299c77ffc93cefd1ad94cb8a3f904e418a
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2024-02-27 11:00:44 +0100

    Add video background to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,27 @@
 <x-app-layout>
+    <video id="video-background" autoplay muted loop>
+        <source src="{{ asset('/vids/Beach_L-to-R.mp4') }}" type="video/mp4">
+        Your browser does not support the video tag.
 
+    </video>
+
+    <script>
+        const videoSources = [
+            "{{ asset('/vids/Beach_L-to-R.mp4') }}",
+            "{{ asset('/vids/Beach_R-to_L.mp4') }}"
+        ];
+        var currentVideo = 0;
+        var video = document.getElementById('video-background');
+
+        function switchVideo() {
+            if (currentVideo === 0) {
+                currentVideo = 1;
+            } else {
+                currentVideo = 0;
+            }
+            video.src = videoSources[currentVideo];
+        }
+
+        setInterval(switchVideo, 25000);
+    </script>
 </x-app-layout>
