27a677a51d240c1fc84645572cc0e645f2446f11
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-02-28 13:19:11 +0100

    created footer component with some basic code

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div class="container">
+        <p class="text-muted">&copy; {{ date('Y') }} Your Company. All rights reserved.</p>
+        <p><a href="{{ url('/privacy-policy') }}">Privacy Policy</a></p>
+    </div>
+</footer>
