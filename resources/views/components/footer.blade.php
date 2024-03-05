4ec8114581d62f0c9ddb1e107cf746cda2b1b8ec
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-03-05 09:29:08 +0100

    basic html structure and added opening hours

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,23 @@
-<footer class="footer">
-    <div class="container">
-        <p class="text-muted">&copy; {{ date('Y') }} Your Company. All rights reserved.</p>
-        <p><a href="{{ url('/privacy-policy') }}">Privacy Policy</a></p>
+<footer>
+    <div class="flex place-content-center space-x-20 text-white">
+        <div>
+            <h3 class="font-bold text-5xl">Contact Info</h3>
+            
+        </div>
+        <div>
+            <h3 class="font-bold text-5xl">Openingstijden</h3>
+            <ul>
+                <li>Maandag: 9:00-20:00</li>
+                <li>Dinsdag: 8:00-20:00</li>
+                <li>Woensdag: 9:00-20:00</li>
+                <li>Donderdag: Gesloten</li>
+                <li>Vrijdag: 12:00-20:00</li>
+                <li>Zaterdag: 10:00-22:00</li>
+                <li>Zondag: 10:00-22:00</li>
+            </ul>
+        </div>
+        <div>
+            <h3 class="font-bold text-5xl">Volg ons</h3>
+        </div>
     </div>
 </footer>
