4c5caac86013fa5e8a61ee7df2632eccd25ccc82
ThePope05 <swobben05@gmail.com>
2024-03-06 09:57:37 +0100

    Add unique name field to invites table

--- database/migrations/2024_03_06_081559_create_invites.php
+++ database/migrations/2024_03_06_081559_create_invites.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('invites', function (Blueprint $table) {
             $table->id();
             $table->boolean('claimed')->default(false);
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('token')->unique();
             $table->timestamps();
