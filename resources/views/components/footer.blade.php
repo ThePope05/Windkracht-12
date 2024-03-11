9dedebc60db0407e4f0d0ab79e11b4656a6b0450
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-03-11 15:05:00 +0100

    added the bottom row and images to: 'volg ons'

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,15 @@
 <footer>
-    <div class="flex place-content-center space-x-20 text-white">
+    <div class="flex place-content-center space-x-20 text-white bg-slate-600">
         <div>
             <h3 class="font-bold text-5xl">Contact Info</h3>
-            
+            <ul>
+                <img src="" alt="">
+                <img src="" alt="">
+            </ul>
         </div>
         <div>
             <h3 class="font-bold text-5xl">Openingstijden</h3>
-            <ul>
+            <ul class="place-self-center">
                 <li>Maandag: 9:00-20:00</li>
                 <li>Dinsdag: 8:00-20:00</li>
                 <li>Woensdag: 9:00-20:00</li>
@@ -18,6 +21,19 @@
         </div>
         <div>
             <h3 class="font-bold text-5xl">Volg ons</h3>
+            <ul class="flex">
+                <img class="w-25 h-20" src="footer-images/twitter.png" alt="">
+                <img class="w-25 h-20" src="footer-images/facebook.png" alt="">
+                <img class="w-25 h-20" src="footer-images/instagram.png" alt="">
+            </ul>
         </div>
+        
     </div>
-</footer>
+    <div class="bg-gray-500 text-white flex w-100vh justify-between">
+        <div class="flex gap-2 items-center">
+            <img class="w-10 h-10" src="footer-images/copyright.png" alt="">
+            <h1 class="place-content-center font-black">Copyright 2024: Designed by Kiteschool</h1>
+        </div>
+        <img class="w-20 h-15" src="footer-images/logo.png" alt="">
+    </div>
+</footer>   
