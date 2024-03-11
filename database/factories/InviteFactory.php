d9eb8db6e9f4da15df71c11b93aea978b6ed5891
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-03-11 16:00:46 +0100

    Added a database seeder for a the admin account

---
+++ database/factories/InviteFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invite>
+ */
+class InviteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'claimed' => false,
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'token' => bin2hex(random_bytes(16)),
+            'last_sent_at' => now(),
+        ];
+    }
+}
