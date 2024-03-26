eb470279f0230521e465170a65b685ae52d3001d
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-03-26 10:09:22 +0100

    added js for navbar scroll

---
+++ public/navbar.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        let navbar = document.querySelector("nav");
+        if (window.scrollY > 110) { 
+            navbar.classList.remove('absolute');
+            navbar.classList.add('fixed');
+            navbar.classList.add('top-0');
+            navbar.classList.remove('mt-28');
+        } else {
+            navbar.classList.remove('fixed');
+            navbar.classList.add('absolute');
+            navbar.classList.remove('top-0');
+            navbar.classList.add('mt-28');
+        } 
+    });
+
+    window.addEventListener("scroll", function() {
+        let navbar = document.querySelector("nav");
+        if (window.scrollY > 300) { 
+            navbar.classList.add("bg-dark-aqua");
+            navbar.classList.add("shadow-xl");
+        } else {
+            navbar.classList.remove("bg-dark-aqua");
+            navbar.classList.remove("shadow-xl");
+        }
+    });
+});
+
